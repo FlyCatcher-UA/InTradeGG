7cb89eaaf21ea222dd07c43e8644bfafb48cbc72
Volodymyr <flycatcherif@gmail.com>
2021-09-09 15:13:13 +0300

    Конвертування, Трай-Кетч, Трай-Парс

---
+++ For_New/For_New5/Lesson5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тест ТрайПарс. Введите число: ");
+            string str2 = Console.ReadLine();
+            int x;
+            int.TryParse(str2, out x);
+            Console.WriteLine($"x = {x}");
+
+            try
+            {
+                Console.Write("Введите число 1: ");
+                string str = Console.ReadLine();
+                int a = Convert.ToInt32(str);
+                int c = int.Parse(str);
+                
+                Console.Write("Введите число 2: ");
+                str = Console.ReadLine();
+                int b = Convert.ToInt32(str);
+                int d = int.Parse(str);
+                Console.WriteLine("Good Convertation");
+                Console.WriteLine($"Сумма = {a+b}; {c+d}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error Convertation");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
