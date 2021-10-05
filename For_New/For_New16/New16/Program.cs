df0c1b586a7812191ee3f331d822a29c04f64574
Volodymyr <flycatcherif@gmail.com>
2021-10-05 15:18:16 +0300

    22 home+

--- For_New/For_New16/New16/Program.cs
+++ For_New/For_New16/New16/Program.cs
@@ -11,17 +11,27 @@ namespace New16
         static void Main(string[] args)
         {
             int counter = 0;
+            int sums = 0;
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
+            int num3 = 0;
+            if (num1>num2)
+            {
+                num3 = num2;
+                num2 = num1;
+                num1 = num3;
+            }
             while (num1 <= num2)
             {
                 if ((num1 % 2) == 0)
                 {
                     counter++;
+                    sums = sums + num1;
                 }
                 num1++;
             }
-            Console.WriteLine($"{counter} - значень кратних 2.");
+            Console.WriteLine($"{counter} - количество четных.");
+            Console.WriteLine($"{sums} - сума четных.");
             Console.ReadKey();
         }
     }
